e55054cadc402a7f7caa9a3d91591f84f923fcae
Xiaonan Shen <sxn@shenxn.io>
2019-07-14 15:14:34 +0800

    Generate domains only

--- _build/build.sh
+++ _build/build.sh
@@ -1,74 +1,13 @@
 mkdir ./_build/tmp/basic -p
 mkdir ./_build/tmp/full -p
-mkdir ./_build/tmp/127.0.0.1/basic -p
-mkdir ./_build/tmp/127.0.0.1/full -p
-echo "Adding version code to head"
-DATE="$(echo $(date +%Y%m%d%H%M%S))"
-echo "#version=$DATE" >> ./_build/head.txt
-echo "#*********************************************************" >> ./_build/head.txt
-echo "Building standard hosts . . ."
+echo "Building hosts with redirecting to 127.0.0.1 . . ."
 find ./_data/basic -type f -name "*.txt" | xargs cat > ./_build/tmp/basic/1.txt
 sed -e '/^#/d' ./_build/tmp/basic/1.txt > ./_build/tmp/basic/2.txt
-cp ./_build/tmp/basic/2.txt ./_build/tmp/basic/3.txt
-sed -i 's|0.0.0.0|::|g' ./_build/tmp/basic/3.txt
-cat ./_build/tmp/basic/2.txt ./_build/tmp/basic/3.txt > ./_build/tmp/basic/4.txt
-sort ./_build/tmp/basic/4.txt -u > ./_build/tmp/basic/5.txt
-cat ./_build/head.txt ./_build/tmp/basic/5.txt > ./_build/tmp/basic/hosts
-echo "Cleaning tmp . . ."
-rm ./_build/tmp/basic/1.txt
-rm ./_build/tmp/basic/2.txt
-rm ./_build/tmp/basic/3.txt
-rm ./_build/tmp/basic/4.txt
-rm ./_build/tmp/basic/5.txt
-
-echo "Building full hosts . . ."
+sed -i 's|0.0.0.0 ||g' ./_build/tmp/basic/2.txt
+sort ./_build/tmp/basic/2.txt -u > ./domains/basic
 find ./_data -type f -name "*.txt" | xargs cat > ./_build/tmp/full/1.txt
 sed -e '/^#/d' ./_build/tmp/full/1.txt > ./_build/tmp/full/2.txt
-cp ./_build/tmp/full/2.txt ./_build/tmp/full/3.txt
-sed -i 's|0.0.0.0|::|g' ./_build/tmp/full/3.txt
-cat ./_build/tmp/full/2.txt ./_build/tmp/full/3.txt > ./_build/tmp/full/4.txt
-sort ./_build/tmp/full/4.txt -u > ./_build/tmp/full/5.txt
-cat ./_build/head.txt ./_build/tmp/full/5.txt > ./_build/tmp/full/hosts
+sed -i 's|0.0.0.0 ||g' ./_build/tmp/full/2.txt
+sort ./_build/tmp/full/2.txt -u > ./domains/full
 echo "Cleaning tmp . . ."
-rm ./_build/tmp/full/1.txt
-rm ./_build/tmp/full/2.txt
-rm ./_build/tmp/full/3.txt
-rm ./_build/tmp/full/4.txt
-rm ./_build/tmp/full/5.txt
-
-echo "Building hosts with redirecting to 127.0.0.1 . . ."
-cp ./_build/tmp/basic/hosts ./_build/tmp/1.txt
-sed -i 's|0.0.0.0|127.0.0.1|g' ./_build/tmp/1.txt
-cp ./_build/tmp/full/hosts ./_build/tmp/2.txt
-sed -i 's|0.0.0.0|127.0.0.1|g' ./_build/tmp/2.txt
-cat ./_build/tmp/1.txt > ./_build/tmp/127.0.0.1/basic/hosts
-cat ./_build/tmp/2.txt > ./_build/tmp/127.0.0.1/full/hosts
-echo "Cleaning tmp . . ."
-rm ./_build/tmp/1.txt
-rm ./_build/tmp/2.txt
-
-echo "Building Mikrotik Basic Shell . . ."
-cat ./_build/tmp/basic/hosts > ./_build/tmp/basic/1.txt
-sed -e '/^#/d' ./_build/tmp/basic/1.txt > ./_build/tmp/basic/2.txt
-sed -e '/^:/d' ./_build/tmp/basic/2.txt > ./_build/tmp/basic/3.txt
-sed -e '/^127.0.0.1/d' ./_build/tmp/basic/3.txt > ./_build/tmp/basic/4.txt
-sed -i 's|0.0.0.0 |ip dns static add address=127.0.0.1 name=|g' ./_build/tmp/basic/4.txt
-cat ./_build/tmp/basic/4.txt > ./_build/tmp/basic/mikrotik.rsc
-echo "Cleaning tmp . . ."
-rm ./_build/tmp/basic/1.txt
-rm ./_build/tmp/basic/2.txt
-rm ./_build/tmp/basic/3.txt
-rm ./_build/tmp/basic/4.txt
-
-echo "Building Mikrotik Full Shell . . ."
-cat ./_build/tmp/full/hosts > ./_build/tmp/full/1.txt
-sed -e '/^#/d' ./_build/tmp/full/1.txt > ./_build/tmp/full/2.txt
-sed -e '/^:/d' ./_build/tmp/full/2.txt > ./_build/tmp/full/3.txt
-sed -e '/^127.0.0.1/d' ./_build/tmp/full/3.txt > ./_build/tmp/full/4.txt
-sed -i 's|0.0.0.0 |ip dns static add address=127.0.0.1 name=|g' ./_build/tmp/full/4.txt
-cat ./_build/tmp/full/4.txt > ./_build/tmp/full/mikrotik.rsc
-echo "Cleaning tmp . . ."
-rm ./_build/tmp/full/1.txt
-rm ./_build/tmp/full/2.txt
-rm ./_build/tmp/full/3.txt
-rm ./_build/tmp/full/4.txt
+rm -rf ./_build/tmp
